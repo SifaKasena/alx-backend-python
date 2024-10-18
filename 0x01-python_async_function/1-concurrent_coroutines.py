d2459419a326da2dc4dd0e30c4948810da9a00d9
SifaKasena <109460248+SifaKasena@users.noreply.github.com>
2024-10-18 13:21:39 +0300

    [Fix:] Annotate return types correctly

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     Executes multiple coroutines concurrently and
     returns their results in the order of completion.
