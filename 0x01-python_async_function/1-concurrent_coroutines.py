b2e671868795b7573e045e34be7135c6dbddc6cc
SifaKasena <109460248+SifaKasena@users.noreply.github.com>
2024-10-18 13:37:30 +0300

    [Fix:] Use typing module for list annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Defines a coroutine that executes multiple coroutines concurrently."""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Executes multiple coroutines concurrently and
     returns their results in the order of completion.
